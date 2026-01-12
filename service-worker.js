bce83d9eff1be8971833c8e89aee88806bc1c3be
jdrestre <jdrestre79@gmail.com>
2026-01-11 19:08:59 -0500

    Corregir rutas para nombre de repositorio correcto

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,50 @@
-const CACHE_NAME = 'marathon-miami-v1';
+const CACHE_NAME = 'marathon-miami-v3';
+const BASE_PATH = '/marathon_miami_2026-';
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './marathon-image.jpg',
-  './icon-192.png',
-  './icon-512.png'
+  `${BASE_PATH}/`,
+  `${BASE_PATH}/index.html`,
+  `${BASE_PATH}/manifest.json`,
+  `${BASE_PATH}/marathon-image.jpg`,
+  `${BASE_PATH}/icon-192.png`,
+  `${BASE_PATH}/icon-512.png`
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cacheando archivos');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.error('Error al cachear:', err))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
